65e41d4113d760b21ba93bceff55f12acb4e6ce6
Kowsiya Srikantharajah <kowsiya.sri@gmail.com>
2024-08-13 16:29:54 -0400

    Add upload functionality

---
+++ server.js
@@ -0,0 +1,43 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import multer from "multer";
+import path from "path";
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (_req, res) => {
+    res.send("Welcome to the PetCare Hub API");
+});
+
+app.post("/api/upload", (req, res) => {
+    res.send("upload successfully");
+});
+
+const storage = multer.diskStorage({
+    destination: (_req, _file, cb) => {
+        cb(null, "uploads/");
+    },
+    filename: (_req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
+    },
+});
+
+const upload = multer({ storage });
+
+app.post("/upload", upload.single("file"), (req, res) => {
+    try {
+        //TODO: add logic for request body form fields
+        res.status(200).send("File uploaded successfully");
+    } catch (error) {
+        res.status(400).send("Error uploading file");
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`);
+});
